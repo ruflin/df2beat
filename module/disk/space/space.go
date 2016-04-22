c3d1929183284ec2dc704e00b8b77e1ab366abbd
Nicolas Ruflin <spam@ruflin.com>
2016-04-22 09:44:56 +0200

    Update to most recent version of metricbeat (#1)
    * Add dependency on topbeat for simplicity
    * Update configuration options

--- module/disk/space/space.go
+++ module/disk/space/space.go
@@ -2,18 +2,58 @@ package space
 
 import (
 	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/logp"
 	"github.com/elastic/beats/metricbeat/helper"
+	"github.com/elastic/beats/topbeat/system"
 )
 
 func init() {
-	helper.Registry.AddMetricSeter("disk", "space", &MetricSeter{})
+	helper.Registry.AddMetricSeter("disk", "space", New)
 }
 
-type MetricSeter struct {}
+// New creates new instance of MetricSeter
+func New() helper.MetricSeter {
+	return &MetricSeter{}
+}
 
-func (m *MetricSeter) Fetch(ms *helper.MetricSet) (events []common.MapStr, err error) {
+type MetricSeter struct{}
 
-	events = eventMapping()
+func (m *MetricSeter) Setup(ms *helper.MetricSet) error {
+	return nil
+}
 
-	return events, nil
+func (m *MetricSeter) Fetch(ms *helper.MetricSet, host string) (event common.MapStr, err error) {
+
+	fss, err := system.GetFileSystemList()
+	if err != nil {
+		logp.Warn("Getting filesystem list: %v", err)
+		return nil, err
+	}
+
+	event = common.MapStr{}
+
+	for _, fs := range fss {
+		fsStat, err := system.GetFileSystemStat(fs)
+		if err != nil {
+			logp.Debug("topbeat", "Skip filesystem %d: %v", fsStat, err)
+			continue
+		}
+
+		fsEvent := common.MapStr{
+			fsStat.DevName: common.MapStr{
+				"device_name": fsStat.DevName,
+				"mount_point": fsStat.Mount,
+				"total":       fsStat.Total,
+				"used":        fsStat.Used,
+				"free":        fsStat.Free,
+				"avail":       fsStat.Avail,
+				"files":       fsStat.Files,
+				"free_files":  fsStat.FreeFiles,
+			},
+		}
+
+		event.Update(fsEvent)
+	}
+
+	return event, nil
 }
